cb09480510383437a9614560335dabe6851072b5
westy12dan <westy12dan@gmail.com>
2024-03-08 15:22:50 +0800

    slashes working

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,41 +8,49 @@ import (
 	"github.com/westbrookdaniel/website/internal/templates"
 )
 
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		templates.Templates.ExecuteTemplate(w, "404.html", nil)
+		return
+	}
+	templates.Templates.ExecuteTemplate(w, "index.html", nil)
+}
+
+func handleBlog(w http.ResponseWriter, r *http.Request) {
+	metas := readMetas()
+	templates.Templates.ExecuteTemplate(w, "blog.html", metas)
+}
+
+func handlePost(w http.ResponseWriter, r *http.Request) {
+	slug := r.PathValue("slug")
+	html, err := os.ReadFile("build/posts/" + slug + ".html")
+	if err != nil {
+		templates.Templates.ExecuteTemplate(w, "404.html", nil)
+		return
+	}
+
+	metas := readMetas()
+	var meta templates.Meta
+	for _, m := range metas {
+		if m.Slug == slug {
+			meta = m
+			break
+		}
+	}
+
+	post := templates.CreatePost(meta, html)
+
+	templates.Templates.ExecuteTemplate(w, "post.html", post)
+}
+
 func main() {
-	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/" {
-			templates.Templates.ExecuteTemplate(w, "404.html", nil)
-			return
-		}
-		templates.Templates.ExecuteTemplate(w, "index.html", nil)
-	})
+	http.HandleFunc("GET /", handleIndex)
 
-	http.HandleFunc("GET /blog", func(w http.ResponseWriter, r *http.Request) {
-		metas := readMetas()
-		templates.Templates.ExecuteTemplate(w, "blog.html", metas)
-	})
+	http.HandleFunc("GET /blog", handleBlog)
+	http.HandleFunc("GET /blog/{$}", handleBlog)
 
-	http.HandleFunc("GET /blog/{slug}", func(w http.ResponseWriter, r *http.Request) {
-		slug := r.PathValue("slug")
-		html, err := os.ReadFile("build/posts/" + slug + ".html")
-		if err != nil {
-			templates.Templates.ExecuteTemplate(w, "404.html", nil)
-			return
-		}
-
-		metas := readMetas()
-		var meta templates.Meta
-		for _, m := range metas {
-			if m.Slug == slug {
-				meta = m
-				break
-			}
-		}
-
-		post := templates.CreatePost(meta, html)
-
-		templates.Templates.ExecuteTemplate(w, "post.html", post)
-	})
+	http.HandleFunc("GET /blog/{slug}", handlePost)
+	http.HandleFunc("GET /blog/{slug}/{$}", handlePost)
 
 	http.ListenAndServe("localhost:3000", nil)
 }
