f7ebd752577428e93758eb1266fb91fcc610fa4f
Daniel Westbrook <westy12dan@gmail.com>
2025-10-29 17:07:02 +0800

    Wip on redo

--- public/index.js
+++ public/index.js
@@ -1,4 +1,10 @@
-let currentTimeout;
+// @ts-check
+/// <reference lib="dom" />
+/// <reference lib="dom.iterable" />
+
+/** @type {import("htmx.org").default} */
+const htmx = window.htmx;
+
 const charTypeSpeeds = [200, 100, 150, 90, 80, 75, 160, 180, 170, 110, 80, 75, 70, 70, 70];
 
 const slugs = [
@@ -20,39 +26,64 @@ const slugs = [
   "website-upgrade",
 ];
 
-htmx.onLoad(() => {
+htmx.onLoad(async () => {
   document.querySelector("#lucky")?.addEventListener("click", () => {
     const randomSlug = slugs[Math.floor(Math.random() * slugs.length)];
     window.location.href = `/blog/${randomSlug}`;
   });
 
   if (window.location.pathname !== "/") return;
-  if (currentTimeout) clearTimeout(currentTimeout);
 
+  const h1 = document.querySelector("h1");
+  const is = document.querySelector("#is");
+  const place = document.querySelector("#place");
+  const wit = document.querySelector("#with");
+  const source = document.querySelector("#source");
+
+  typeElement(h1);
+  (async () => {
+    await typeElement(is);
+    await typeElement(place);
+  })();
+  (async () => {
+    await typeElement(wit);
+    await typeElement(source);
+  })();
+});
+
+/** @type {WeakMap<HTMLElement, NodeJS.Timeout>} */
+let timeouts = new WeakMap();
+
+/** @param {HTMLElement} el */
+async function typeElement(el) {
+  const current = timeouts.get(el);
+  if (current) clearTimeout(current);
+
+  const chars = smartSplit(el.ariaLabel ?? "");
+  el.innerText = "";
+
+  return new Promise((resolve) => {
+    const next = setTimeout(() => {
+      function type() {
+        const charIndex = el.innerText.length % charTypeSpeeds.length;
+        return setTimeout(() => {
+          el.innerText += chars.shift();
+          if (chars.length === 0) resolve();
+          else type();
+        }, charTypeSpeeds[charIndex]);
+      }
+      timeouts.set(el, type());
+    }, 200);
+
+    timeouts.set(el, next);
+  });
+}
+
+function smartSplit(str) {
   let s = "";
-  const chars = "Daniel Westbrook".split("").reduce((acc, c) => {
+  return str.split("").reduce((acc, c) => {
     if (c.trim() === "") s = c;
     else acc.push(s + c) && (s = "");
     return acc;
   }, []);
-
-  const h = document.querySelector("h1");
-  const c = document.querySelector("#cursor");
-
-  h.innerText = "";
-
-  currentTimeout = setTimeout(() => {
-    function type() {
-      const charIndex = h.innerText.length % charTypeSpeeds.length;
-      setTimeout(() => {
-        h.innerText += chars.shift();
-        if (chars.length == 0) {
-          setInterval(() => (c.hidden = !c.hidden), 700);
-        } else {
-          type();
-        }
-      }, charTypeSpeeds[charIndex]);
-    }
-    currentTimeout = type();
-  }, 400);
-});
+}
