f49f95cb7cb8e2983367ec6269cdd95b2a3dedd5
Daniel Westbrook <westy12dan@gmail.com>
2026-01-09 15:49:05 +0800

    checkpoint

--- public/index.js
+++ public/index.js
@@ -5,8 +5,6 @@
 /** @type {import("htmx.org").default} */
 const htmx = window.htmx;
 
-const charTypeSpeeds = [200, 100, 150, 90, 80, 75, 160, 180, 170, 110, 80, 75, 70, 70, 70];
-
 const slugs = [
   "abstractions-and-simplicity",
   "audit-react-performance",
@@ -31,60 +29,4 @@ htmx.onLoad(async () => {
     const randomSlug = slugs[Math.floor(Math.random() * slugs.length)];
     window.location.href = `/blog/${randomSlug}`;
   });
-
-  if (window.location.pathname !== "/") return;
-
-  const h1 = document.querySelector("h1");
-  const is = document.querySelector("#is");
-  const place = document.querySelector("#place");
-  const wit = document.querySelector("#with");
-  const source = document.querySelector("#source");
-
-  typeElement(h1);
-  setTimeout(async () => {
-    await typeElement(is);
-    setTimeout(async () => {
-      typeElement(place);
-      await typeElement(wit);
-      typeElement(source);
-    }, 500);
-  });
 });
-
-/** @type {WeakMap<HTMLElement, NodeJS.Timeout>} */
-let timeouts = new WeakMap();
-
-/** @param {HTMLElement} el */
-async function typeElement(el) {
-  const current = timeouts.get(el);
-  if (current) clearTimeout(current);
-
-  const chars = smartSplit(el.ariaLabel ?? "");
-  el.innerText = "";
-
-  return new Promise((resolve) => {
-    const next = setTimeout(() => {
-      function type() {
-        const charIndex = el.innerText.length % charTypeSpeeds.length;
-
-        return setTimeout(() => {
-          el.innerText += chars.shift();
-          if (chars.length === 0) resolve();
-          else type();
-        }, charTypeSpeeds[charIndex] / 1.5);
-      }
-      timeouts.set(el, type());
-    }, 200);
-
-    timeouts.set(el, next);
-  });
-}
-
-function smartSplit(str) {
-  let s = "";
-  return str.split("").reduce((acc, c) => {
-    if (c.trim() === "") s = c;
-    else acc.push(s + c) && (s = "");
-    return acc;
-  }, []);
-}
